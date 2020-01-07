b79741d61dbe5cacfb7f0410033c4bb963cab9cb
Vitaly Zharov <zharov.vitalik@gmail.com>
2020-01-07 13:29:24 +0500

    Добавлены основные зависимости в pom.xml. Подготовлена общая структура проекта.

---
+++ src/main/java/com/github/zharovvv/springwebapp/dao/UserDAO.java
@@ -0,0 +1,4 @@
+package com.github.zharovvv.springwebapp.dao;
+
+public interface UserDAO {
+}
---
+++ src/main/java/com/github/zharovvv/springwebapp/service/UserService.java
@@ -0,0 +1,4 @@
+package com.github.zharovvv.springwebapp.service;
+
+public interface UserService {
+}
